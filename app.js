eba879a3dcba5d94435699783ada27e9b8c9f113
ashaduzzaman10 <devakashtbo10@gmail.com>
2024-11-24 01:47:11 +0600

    feat : user manegement and cheking session (done)

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const passportConfig = require("./config/passport/passport");
 
 const app = express();
 
-
 // Application middleware
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -25,18 +24,18 @@ app.use(express.urlencoded({ extended: true }));
 // Session management
 app.set("trust proxy", 1);
 app.use(
-    session({
-        secret: "keyboard cat",
-        resave: false,
-        saveUninitialized: true,
-        store: MongoStore.create({
-            mongoUrl: process.env.DBURL,
-            collectionName: "session",
-        }),
-        cookie: {
-            secure: false, 
-        },
-    })
+	session({
+		secret: "keyboard cat",
+		resave: false,
+		saveUninitialized: true,
+		store: MongoStore.create({
+			mongoUrl: process.env.DBURL,
+			collectionName: "session",
+		}),
+		cookie: {
+			secure: false,
+		},
+	})
 );
 
 // Passport session management
@@ -45,71 +44,100 @@ app.use(passport.session());
 
 // Routes
 app.get("/register", (req, res) => {
-    res.render("register");
+	res.render("register");
 });
 
 app.post("/register", async (req, res) => {
-    try {
-        const { username, password } = req.body;
-        const existingUser = await UserModel.findOne({ userName: username });
-        if (existingUser) {
-            return res.status(400).json({ message: "User already exists" });
-        }
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-        const newUser = new UserModel({ userName: username, password: hashedPassword });
-        await newUser.save();
-        res.status(201).redirect("/login");
-    } catch (error) {
-        res.status(500).json({ success: false, data: { error: error.message } });
-    }
+	try {
+		const { username, password } = req.body;
+		const existingUser = await UserModel.findOne({ userName: username });
+		if (existingUser) {
+			return res.status(400).json({ message: "User already exists" });
+		}
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+		const newUser = new UserModel({
+			userName: username,
+			password: hashedPassword,
+		});
+		await newUser.save();
+		res.status(201).redirect("/login");
+	} catch (error) {
+		res.status(500).json({ success: false, data: { error: error.message } });
+	}
 });
 
-app.get("/login", (req, res) => {
-    res.render("login");
+// auth checker
+
+const checkLogIn = (req, res, next) => {
+	if (req.isAuthenticated()) {
+		return res.redirect("/profile");
+	}
+	next();
+};
+
+app.get("/login", checkLogIn, (req, res) => {
+	res.render("login");
 });
 
 app.post(
-    "/login",
-    passport.authenticate("local", {
-        failureRedirect: "/login",
-        successRedirect: "/profile",
-    })
+	"/login",
+	passport.authenticate("local", {
+		failureRedirect: "/login",
+		successRedirect: "/profile",
+	})
 );
 
 app.get("/logout", (req, res) => {
-    req.logout((err) => {
-        if (err) {
-            return next(err);
-        }
-        res.redirect("/");
-    });
+	try {
+		req.logout((err) => {
+			if (err) {
+				return next(err);
+			}
+			res.redirect("/");
+		});
+	} catch (error) {
+		res.status(500).json({
+			message: error.message,
+		});
+	}
 });
 
-app.get("/profile", (req, res) => {
-    if (!req.isAuthenticated()) {
-        return res.redirect("/login");
+// check auth user 
+const checkAuthenticateUser = ( req, res, next ) => {
+    if ( req.isAuthenticated() ) {
+        return next();
     }
-    res.render("profile");
+    res.redirect("login")
+}
+
+app.get("/profile", checkAuthenticateUser,(req, res) => {
+	if (!req.isAuthenticated()) {
+		return res.redirect("/login");
+	}
+	res.render("profile");
 });
 
 app.get("/health", (req, res) => {
-    res.status(200).json({ success: true, data: { message: "ok,success" } });
+	res.status(200).json({ success: true, data: { message: "ok,success" } });
 });
 
 app.get("/", (req, res) => {
-    res.render("index");
+	res.render("index");
 });
 
 // Error handlers
 app.use((req, res, next) => {
-    const error = new Error("Not Found");
-    error.status = 404;
-    next(error);
+	const error = new Error("Not Found");
+	error.status = 404;
+	next(error);
 });
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500).json({ success: false, data: { message: error.message || "Server error" } });
+	res.status(error.status || 500).json({
+		success: false,
+		data: { message: error.message || "Server error" },
+	});
 });
 
 module.exports = app;
